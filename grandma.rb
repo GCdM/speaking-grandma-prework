2fa4a1bdb10f72f1540f0998c6060d9df2b454f9
Gabriel Miranda <gabrielcm.br@gmail.com>
2018-05-24 14:05:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(say)
   if (say == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif (say == say.UpperCase)
+  elsif (say == say.toUpperCase)
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
